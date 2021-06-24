a8e60e6986b0897422fb56565c14897d879e140a
RohanDey02 <58871401+RohanDey02@users.noreply.github.com>
2021-06-24 00:58:04 -0400

    Update balanceDistributions.py Day 2
    This is the work done for Day 2

--- balanceDistributions.py
+++ balanceDistributions.py
@@ -1,80 +1,149 @@
 import country as c
+import math
 from country import Country
+from operator import attrgetter
+
+# Global Vars
 
 
-def find_negative_country() -> Country:
-    for country in c.country_master_list.reverse:
-        if country.surplus < 0:
-            return country
+# def find_negative_country(curr_country: Country) -> Country:
+#     temp_list = c.country_master_list
+#     temp_list.reverse
+#     for country in temp_list:
+#         if country.surplus() < 0:
+#             return country
 
 
-def balance():
-    """
-    country_to_quantity = {}
+# def find_poor_country(curr_country: Country) -> Country:
+#     temp_master_list = c.country_master_list
+#     temp_master_list.sort(key=attrgetter("gdp"))
+#     for country in temp_master_list:
+#         if country.surplus() < 2:
+#             return country
+def test_total_vaccines():
+    total = 0
     for country in c.country_master_list:
-        country_to_quantity[country.name] = country.surplus()
+        total += country.num_vaccines
+    return total
 
-    sorted(country_to_quantity.values())
 
-    for country in country_to_quantity:
-        while country.surplus > 0:
-            donatee = find_negative_country
-            c.country.donate(donatee.name, 1)
+def balance() -> int:
+    """ 
+    The balance function redistributes vaccines fairly and is done in two stages:
+    Returns the amount of vaccines needed to balance
+    Stage 1:
+    - Calculate the total amount of vaccine surplus
+    - Divide the surplus by the amount of countries in a deficit
+    - Distribute the vaccines to the deficit countries equally
+        * If one country has less deficit than the amount distributed to them
+        only give the amount they need.
+    If after distribution there are extra surplus left, the surplus will
+    be redistributed to the surplus countries.
+    *** IMPORTANT ***
+    The distribution plot will be outputted after this stage.
+
+
+    Stage 2:
+    If there are still deficits:
+    Calculate the amount of vaccines needed to balance,
+    Include this amount in final graph.
     """
 
-    # Redistribution algorithm: Porom Rendition
+    #STAGE 1
     deficit_countries = {}
     surplus_countries = {}
 
-    # Find the countries that are in a deficit of vaccines and surpluses
+    # # Find the countries that are in a deficit of vaccines and surpluses
     for country in c.country_master_list:
-        if country.surplus() < 0:
-            deficit_countries[country] = country.surplus()
-        else:
-            surplus_countries[country] = country.surplus()
+         if country.surplus() < 0:
+             deficit_countries[country] = country.surplus()
+         else:
+             surplus_countries[country] = country.surplus()
     
     #order the countries from greatest to least deficits
     deficit_countries = {k: v for k, v in sorted(deficit_countries.items(), key=lambda item: item[1])}
     #order the countries from greatest to least surplus's
     surplus_countries = {k: v for k, v in sorted(surplus_countries.items(), key=lambda item: item[1], reverse=True)}
+    print("Total vaccines in the system: %d", test_total_vaccines())
+    total_surplus = 0
+    #Find total amount of surplus
+    for country in surplus_countries:
+        total_surplus += surplus_countries[country]
+        #Take away the surplus vaccines from the countries to stock pile
+        country.num_vaccines -= country.surplus()
 
-    # Donate to countries in deficit in priority order from greatest deficit to least
-    # Donatees donate based on highest surplus to least
-    print("Deficit countries before balancing: ")
+
+    """
+    #TEST PRINT OUT DATA
+    print("Surplus countries before distribution")
+    for country in surplus_countries:
+        print(country.name + " " + str(surplus_countries[country]))
+    print("Deficit countries before distribution")
     for country in deficit_countries:
         print(country.name + ": " + str(deficit_countries[country]))
-    print("Surplus countries before balancing: ")
+    """
+    
+    print("Vaccine rates before distribution: ")
+    for country in c.country_master_list:
+        print(country.name + ": " + str(country.vaccination_rate())) 
+
+
+    #Find the average distribution amount to each deficit country
+    while(len(deficit_countries) > 0 and total_surplus > 0):
+        average_distribution = math.floor(float(total_surplus/len(deficit_countries)))
+        print("Average distribution: %d, Amount of donatees: %d, Total surplus: %d" % (average_distribution, len(deficit_countries), total_surplus))
+        donated_counter = 0
+        for country in deficit_countries:
+            donated_counter += 1
+            if average_distribution >= -1 * country.surplus():
+                country.num_vaccines += -1 * deficit_countries[country]
+                deficit_countries[country] = 0
+                total_surplus -= -1 * deficit_countries[country]
+            else:
+                deficit_countries[country] += average_distribution
+                country.num_vaccines += average_distribution
+                total_surplus -= average_distribution
+        #Remove non deficit countries from deficit countries
+        for i in deficit_countries.copy():
+            if deficit_countries[i] == 0:
+                deficit_countries.pop(i)
+    """
+    #Assume that every surplus country donated their surplus, set the values accordingly
     for country in surplus_countries:
-        print(country.name + ": " + str(surplus_countries[country]))
+        country.num_vaccines -= country.surplus()
+    """
     
+    #Equally give back the number of vaccines that may be left over in the surplus
+    #To the countries that donated
+    if total_surplus > 0:
+        average_distribution = math.floor(float(total_surplus/len(surplus_countries)))
+        for country in surplus_countries:
+            country.num_vaccines += average_distribution
     
-    for demand in deficit_countries:
-        for supply in surplus_countries:
-            if -1 * deficit_countries[demand] <= surplus_countries[supply] and deficit_countries[demand] != 0:
-                print("entered for:"+ demand.name + "," + supply.name)
-                #Execute donation
-                supply.donate(demand, deficit_countries[demand])
-                #update dictionaries
-                surplus_countries[supply] += deficit_countries[demand]
-                deficit_countries[demand] = 0
-            elif deficit_countries[demand] > surplus_countries[supply]:
-                #print("entered 2nd for:"+ demand.name + "," + supply.name)
-                #donate entire supply
-                supply.donate(demand, surplus_countries[supply])
-                #update dictionaries
-                deficit_countries[demand] += surplus_countries[supply]
-                surplus_countries[supply] = 0
-    print("Deficit countries after balancing: ")
+    print("Vaccine rates after distribution: ")
+    for country in c.country_master_list:
+        print(country.name + ": " + str(country.vaccination_rate())) 
+
+    print("Total vaccines in the system: %d", test_total_vaccines())
+    """
+    print("Surplus countries after distribution")
+    for country in surplus_countries:
+        print(country.name + " " + str(country.num_vaccines))
+    print("Deficit countries after distribution")
     for country in deficit_countries:
         print(country.name + ": " + str(deficit_countries[country]))
-    print("Surplus countries after balancing: ")
-    for country in surplus_countries:
-        print(country.name + ": " + str(surplus_countries[country]))
-                
-                
+    """
+    #STAGE 2: find the amount of vaccines needed to purchase
+    # To properly balance the graph
+    if len(deficit_countries) > 0:
+        #Find total deficit:
+        return -1 * sum(deficit_countries.values())
+    else:
+        return 0
+
 
 if __name__ == "__main__":
+    #pp.master_list_to_csv(country_to_quantity=country_to_quantity)
     balance()
-
 
 # https://ourworldindata.org/covid-vaccinations
